2fdfc5521ba5c943dc5f3420fa480cbd51958a17
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-11-25 22:56:52 +0900

    order フォームオブジェクト未完成

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,5 +8,17 @@ class Order < ApplicationRecord
     validates :user_id
     validates :recruitment_id
   end
+
+  # def save
+  #   donation = Donation.create(price: price, user_id: user_id)
+  #   # 住所を保存する
+  #   # donation_idには、変数donationのidと指定する
+  #   Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
+  # end
+
+  def save
+    purchase_record = PurchaseRecord.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number, purchase_record_id: purchase_record.id)
+  end
 end
 
